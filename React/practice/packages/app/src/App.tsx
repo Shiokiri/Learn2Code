82caf0b44f0516cd90035c4a5990349b18032f3a
Shiokiri <xluoying2017@gmail.com>
2023-10-04 09:45:53 +0800

    feat: import mui

---
+++ React/practice/packages/app/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./assets/logo.svg";
+import "./App.css";
+import SignUp from "./components/SignUp";
+import SignInSide from "./components/SignInSide";
+
+function App() {
+  return (
+    <>
+      <SignUp />
+      <SignInSide />
+    </>
+  );
+}
+
+export default App;
