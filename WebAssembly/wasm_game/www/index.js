370ef0081265ce2dc3aa9d9ebe77460ab783ae5f
Shiokiri <xluoying2017@gmail.com>
2023-10-20 21:37:15 +0800

    feat: try wat call js function

--- WebAssembly/wasm_game/www/index.js
+++ WebAssembly/wasm_game/www/index.js
@@ -1,10 +1,22 @@
 async function loadWasm() {
-  const response = await fetch("test.wasm");
-  const buffer = await response.arrayBuffer();
-  const wasm = await WebAssembly.instantiate(buffer);
+  const importObject = {
+    console: {
+      log: () => {
+        console.log("log called");
+      },
+      error: () => {
+        console.log("error called");
+      },
+    },
+  };
 
-  const addTwo = wasm.instance.exports.addTwo;
-  const result = addTwo(1, 2);
+  const response = await fetch("add.wasm");
+  const buffer = await response.arrayBuffer();
+  debugger;
+  const wasm = await WebAssembly.instantiate(buffer, importObject);
+
+  const add = wasm.instance.exports.add;
+  const result = add(1, 2);
 
   console.log(result);
 }
